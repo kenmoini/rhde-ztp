f3a3260fa4580d9231156b6e0a286fff3056af14
Ken Moini <ken@kenmoini.com>
2024-07-10 07:04:29 -0400

    add fdo image builder and post config automation

---
+++ ansible/files/fdo/etc/glue/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# - FDO Provisions device-side one-shot service
+# - Device-side One-shot service runs script
+# - Device-side Glue script gets system MAC Address, and sends it to a Glue Service
+# - Hosted Glue Service looks up Job Code data and MAC Address submitted by field tech
+# - Hosted Glue Service configures Ansible Inventory with host info from Job Code data
+# - Hosted Glue Service runs Ansible playbook to configure system
+
+export GLUE_SERVICE="https://fdo-ansible-glue-api.kemo.labs"
+
+export DEFAULT_ROUTE_DEVICE=$(ip route show default | awk '/default/ {print $5}')
+
+export DEFAULT_MAC_ADDRESS=$(cat /sys/class/net/${DEFAULT_ROUTE_DEVICE}/address)
+
+export DEFAULT_IP_ADDRESS=$(ip addr show ${DEFAULT_ROUTE_DEVICE} | awk '/inet / {print $2}' | cut -d/ -f1)
+
+# Post the MAC address to the Glue API
+curl -X POST -H "Content-Type: application/json" -d "{\"mac_address\": \"$DEFAULT_MAC_ADDRESS\", \"provisioned_ip\": \"$DEFAULT_IP_ADDRESS\", \"default_device\": \"$DEFAULT_ROUTE_DEVICE\"}" $GLUE_SERVICE/api/v1/system-up
